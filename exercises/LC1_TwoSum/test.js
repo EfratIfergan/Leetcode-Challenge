a4d93f81866b9eaea0e58ef6dc65e34a1870cf69
EfratIfergan <efrat3888@gmail.com>
2023-03-04 22:48:09 +0200

    Test

--- exercises/LC1_TwoSum/test.js
+++ exercises/LC1_TwoSum/test.js
@@ -7,6 +7,4 @@ test("twoSum([2,7,11,15], 9) returns [0,1]", () => {
   ]);
 });
 
-test("twoSum([3, 2, 4], 6) returns [1,2]", () => {
-  expect(twoSum([3, 2, 4], 6).sort((a, b) => (a < b ? -1 : 1))).toEqual([1, 2]);
-});
+
